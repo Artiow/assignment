b4845bc602b12ab59b027d47920165b269eac346
xcode <xcode@c1r009n11.amm.vsu.ru>
2018-11-07 13:01:24 +0300

    game template add

---
+++ TheSetGame/SetGame.swift
@@ -0,0 +1,42 @@
+//
+//  SetGame.swift
+//  TheSetGame
+//
+//  Created by xcode on 07.11.2018.
+//  Copyright © 2018 VSU. All rights reserved.
+//
+
+import Foundation
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
+}
+
+extension MutableCollection {
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let i = index(firstUnshuffled, offsetBy: d)
+            swapAt(firstUnshuffled, i)
+        }
+    }
+}
+
+extension Sequence {
+    func shuffled() -> [Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
+class SetGame {
+    
+    init() {
+    }
+}
